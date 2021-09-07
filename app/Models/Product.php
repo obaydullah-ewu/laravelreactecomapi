e11d36460d4b90afdc7b220d787719a1756b2857
Md. Obaydullah <obaydullah@parallaxlogic.net>
2021-09-07 11:19:06 +0600

    a

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,6 +12,7 @@ class Product extends Model
     protected  $with = ['category'];
     public function category()
     {
+
         return $this->belongsTo(Category::class, 'category_id', 'id');
     }
 }
